c344bc636eb254a74461c45373cbedfe94381cad
Crystal Mackey <moonrunner575@gmail.com>
2022-02-17 17:07:08 -0500

    add headline, skills and social icons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import About from './components/about/aboutMe';
 
 
 
 function App() {
   return (
     <div className="App">
-      
+      <About/>
     </div>
   );
 }
---
+++ src/components/about/aboutMe.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+
+
+
+class About extends Component {
+    render() {
+        return(<div className='head' style={{width: '100%', margin: 'auto'}}>
+        <div className="landing-grid">
+          <div>
+            <div className="banner-text">
+              <h1>React Web Developer</h1>
+
+            {/* <hr/> */}
+
+          <p>HTML/CSS | Bootstrap | JavaScript | React | NodeJS | Express | MongoDB</p>
+
+        <div className="social-links">
+        <a href='https://github.com/Crystal-Mackey'><i class="fa fa-github fa-2x"></i></a>
+        <a href='https://www.linkedin.com/in/crystal-mackey-02763a102/'><i class="fa fa-linkedin fa-2x"></i></a>
+        <a href='https://www.facebook.com/crystal.mackey.397'><i class="fa fa-facebook fa-2x"></i></a>
+
+        
+
+        </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+
+export default About;
